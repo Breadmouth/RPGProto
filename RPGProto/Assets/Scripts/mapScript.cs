b7e064c7885cb2fd5f62d8debbcdbc457c7db9d9
Breadmouth <ipspdowling@gmail.com>
2015-08-25 14:33:04 +1000

    Initial commit

---
+++ RPGProto/Assets/Scripts/mapScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Tile
+{
+	EMPTY,
+	WALL,
+	PLAYER,
+	ENEMY,
+};
+
+public class mapScript : MonoBehaviour {
+
+	public Tile map;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
